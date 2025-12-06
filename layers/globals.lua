65a95b94b045be336e8a7c9fb42328d784e98ef5
Judsondward <judsondward@gmail.com>
2025-12-06 07:59:54 -0600

    removed layer to utilize submodule functionality

--- layers/globals.lua
+++
@@ -1,9 +0,0 @@
----
--- Stores data that that multiple modules need access to.
-local globals = {}
-
-globals.ModeGroups = {}
-globals.CurrentEventHandler = function() end
-globals.LastPlayerLevel = 0
-
-return globals
